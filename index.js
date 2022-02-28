6140594ad4ff85e7b90f8f573f074c9d35de46aa
brian ngeno <bkn.ngeno@gmail.com>
2022-02-28 21:22:26 +0300

    assignment complete

---
+++ index.js
@@ -0,0 +1,16 @@
+
+
+function receivesAFunction(callback) {
+    callback();
+  }
+  
+
+function  returnsANamedFunction (){
+    return function namedFunction(){
+        console.log('this is the returned named function');
+    }
+}
+
+function returnsAnAnonymousFunction() { 
+    return () => console.log('ananonymous function returned');
+};
